7544991a20314c8fa48e0a9372926a2c52a91b51
J <59713685+josevflores911@users.noreply.github.com>
2023-10-29 13:26:05 -0300

    frontend part 1 todo

---
+++ fundamentos-react/frontend/src/about/about.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import PageHeader from '../template/pageHeader'
+
+export default props =>{
+    return(
+        <div>
+            <PageHeader name='Sobre' small='Nosotros'></PageHeader>
+
+            <h2>Nuestra historia</h2>
+            <p>Lorem ipsum dolor sit amet...</p>
+            <h2>Mision y Vision</h2>
+            <p>Lorem ipsum dolor sit amet...</p>
+            <h2>Contacto</h2>
+            <p>Lorem ipsum dolor sit amet...</p>
+        </div>
+    )
+}
