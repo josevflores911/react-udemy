ad22bebf72a80771a9708aecd3bebedef900c0fb
J <59713685+josevflores911@users.noreply.github.com>
2023-10-31 15:33:30 -0300

    front end part 2

--- fundamentos-react/frontend/src/todo/todoForm.jsx
+++ fundamentos-react/frontend/src/todo/todoForm.jsx
@@ -1,9 +1,27 @@
 import React from "react";
+import Grid from '../template/grid'
+import IconButton from "../template/iconButton";
 
 export default props =>{
     return(
-        <div>
-            <h1>Form</h1>
+        <div role='form' className="todoForm">
+            <Grid cols='12 9 10'>
+                <input id="description" className="form-control" 
+                placeholder="Agregue una nueva tarea" 
+                onChange={props.handleChange}
+                value={props.description}
+                />
+            </Grid>
+            <Grid cols='12 3 2'>
+               <IconButton style='primary' icon='plus' onClick={props.handleAdd}></IconButton>
+                {/* <button className="btn btn-primary">
+                    <i className="fa fa-plus"></i>
+                </button> */}
+            </Grid>
+            {/* <div className="col-xs-12 col-sm-9 col-md-10">
+            </div>
+            <div className="col-xs-12 col-sm-3 col-md-2">
+            </div>  */}
         </div>
     )
 }
