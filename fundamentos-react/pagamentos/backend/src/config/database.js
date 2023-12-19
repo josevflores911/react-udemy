65a56a8e0ce829a378e86bcefb20843cc818228b
josevflores911 <josevflores911@gmail.com>
2023-12-19 10:09:16 -0300

    pagametos backend start

---
+++ fundamentos-react/pagamentos/backend/src/config/database.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+mongoose.Promise = global.Promise
+mongoose.connect('mongodb+srv://josevflores911:19274746@cluster0.lta12.mongodb.net/pagamentos');
+const objectDb =  mongoose.connection
+objectDb.on('connected', ()=>{
+    console.log('Conexion correcta a Mongo')
+})
+objectDb.on('error', ()=>{
+    console.log('Erro en conexion a Mongo')
+})
+module.exports = mongoose
